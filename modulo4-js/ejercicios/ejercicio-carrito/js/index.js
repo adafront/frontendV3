648c47d4cd6875f9b2c758c65f19d2cdc51789b5
ada-lenovo-1 <ada@ada-lenovo-1.ada.com.ar>
2018-05-03 12:55:11 -0300

    avances carrito

--- modulo4-js/ejercicios/ejercicio-carrito/js/index.js
+++ modulo4-js/ejercicios/ejercicio-carrito/js/index.js
@@ -1,6 +1,15 @@
 // Creo un objeto producto vacio para guardar la información
 var producto = {};
 
+var carritoStorage = localStorage.getItem("carrito");
+
+if(carritoStorage == null){
+
+	var carrito = [];
+}else{
+	carrito = JSON.parse(carritoStorage).productos;
+}
+
 
 $('.add').on('click',function(e){
 
@@ -8,19 +17,32 @@ $('.add').on('click',function(e){
 
 	// Recupero la información del HTML
 
-	producto.url = hermanos[0].src;
+ 	producto.url = hermanos[0].src;
 	producto.descripcion = hermanos[1].innerText;
 	producto.precio = hermanos[2].innerText;
-	
-	// Creo un objeto item, para agregar luego al carrito
+	producto.stock = hermanos[3].innerText;
 
-	var item ={
-		cantidad : hermanos[3].value,
-		producto : producto
-	}
-		
-	console.log(item);
+	if(parseInt(producto.stock) >= parseInt(hermanos[4].value)){
+
+		// Creo un objeto item, para agregar luego al carrito
+
+		var item ={
+			cantidad : parseInt(hermanos[4].value),
+			producto : producto
+		}
+			
+		console.log(item);
+
+		carrito.push(item);
+
+		let jsonCarrito = {'productos': carrito};
+		localStorage.setItem('carrito',JSON.stringify(jsonCarrito));
+	
+	}else{
+		alert("No se agregó el producto, no hay stock suficiente.");
+	}	
 
 });
+
 
 
