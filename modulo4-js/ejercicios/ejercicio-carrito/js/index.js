ef0d53f5b072550cced4ab3018467632f3d2b95f
ada-lenovo-1 <ada@ada-lenovo-1.ada.com.ar>
2018-05-04 13:08:13 -0300

    funciones adicionales resolucion de dudas

--- modulo4-js/ejercicios/ejercicio-carrito/js/index.js
+++ modulo4-js/ejercicios/ejercicio-carrito/js/index.js
@@ -10,6 +10,28 @@ if(carritoStorage == null){
 	carrito = JSON.parse(carritoStorage).productos;
 }
 
+/***
+  * Verifico si ya existe el producto en el carrito
+  * @params producto
+  * Return boolean | int
+***/  
+function chequearExistencia(producto){
+
+	let cod = producto.codigo; // variable auxiliar
+	let encontrado = false;
+	let i = 0;
+
+	while(!encontrado && i<carrito.length){ // !(encontrado == true)
+
+		if(carrito[i].producto.cod == cod){
+			 return i; // devuelvo la posicion
+		}
+		i++;
+	}
+	// devuelve false si no lo encontro
+	return encontrado;
+}
+
 
 $('.add').on('click',function(e){
 
@@ -21,19 +43,30 @@ $('.add').on('click',function(e){
 	producto.descripcion = hermanos[1].innerText;
 	producto.precio = hermanos[2].innerText;
 	producto.stock = hermanos[3].innerText;
+	producto.cod = hermanos[4].innerText;
 
-	if(parseInt(producto.stock) >= parseInt(hermanos[4].value)){
+	if(parseInt(producto.stock) >= parseInt(hermanos[5].value)){	
+		
+		let pos = chequearExistencia(producto);
 
-		// Creo un objeto item, para agregar luego al carrito
+		if(!pos){
 
-		var item ={
-			cantidad : parseInt(hermanos[4].value),
-			producto : producto
-		}
-			
-		console.log(item);
+			// Creo un objeto item, para agregar luego al carrito
 
-		carrito.push(item);
+			var item ={
+				cantidad : parseInt(hermanos[5].value),
+				producto : producto
+			}
+
+			console.log(item);
+
+			carrito.push(item);
+		
+		}else{
+
+			carrito[pos].cantidad += parseInt(hermanos[5].value);
+
+		}	
 
 		let jsonCarrito = {'productos': carrito};
 		localStorage.setItem('carrito',JSON.stringify(jsonCarrito));
@@ -43,6 +76,35 @@ $('.add').on('click',function(e){
 	}	
 
 });
+
+/**
+  * Devuelve el monto total del carrito
+  **/
+
+function calcularSubtotal(){
+
+	let subtotal = 0;
+
+	for(i=0;i<carrito.length;i++){
+
+		subtotal += carrito[i].cantidad * parseFloat(carrito[i].producto.precio);
+	}
+
+	return subtotal;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
