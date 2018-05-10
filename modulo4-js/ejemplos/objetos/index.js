a27af6630e535e60c9f4b29cbd2e498e23f7cb93
ada-lenovo-1 <ada@ada-lenovo-1.ada.com.ar>
2018-05-10 08:53:05 -0300

    modificado en clase

--- modulo4-js/ejemplos/objetos/index.js
+++ modulo4-js/ejemplos/objetos/index.js
@@ -1,24 +1,73 @@
 class Perro {
 
+  /** Plan A **/	
+
   constructor ( x = "", y = "" ) {
     this._nombre = x;
     this._raza = y;
+    this._edad = 2;
+    this._castrado = false;
   }
+
+  // Debe definirse el constructor o con la palabra constructor 
+  // o utilizando el mismo nombre de la clase
+
+/*	  
+  Perro( x = "", y = "" ) {
+    this._nombre = x;
+    this._raza = y;
+  }
+*/
+
+ // setters y getters	
 
   set nombre ( value ) {
     this._nombre = value;
   }
 
+  set raza(value){
+  	this._raza = value;
+  }
+
+  /**
+  *   Retorna siempre una cadena de caracteres	
+  *
+  **/
+
   toString () {
     return this._nombre + ', ' + this._raza;
+  }
+
+  getRaza (){
+  	return this._raza;
+  }
+
+  /**
+  	* Returns int 
+  **/ 
+
+  getEdad(){
+  	return this._edad;
   }
 }
 
 // Creacion de objetos
 let p1 = new Perro("Fido","Bull terrier");
-let p2 = new Perro();
+let p3 = new Perro();
 
 
-p2.toString(); 
+let salida = p1.toString(); 
 
-console.log(p1.toString());
+console.log(salida);
+
+
+
+
+
+
+
+
+
+
+
+
