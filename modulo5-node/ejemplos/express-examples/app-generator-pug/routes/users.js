2c0b617bf91f5de469269bf5ac6c9fbc66d713e5
ada-lenovo-1 <ada@ada-lenovo-1.ada.com.ar>
2018-05-29 11:05:49 -0300

    views con pug

--- modulo5-node/ejemplos/express-examples/app-generator-pug/routes/users.js
+++ modulo5-node/ejemplos/express-examples/app-generator-pug/routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  res.render('user', 
+  	{ message: 'Una peticion a usuario'});
 });
 
 module.exports = router;
